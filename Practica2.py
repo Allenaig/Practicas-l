40e30f8122f9db95465f4c1b9231bfa0cb402b2a
Allenaig <1507828@senati.pe>
2024-10-16 00:37:14 +0000

    add:arroba

--- Practica2.py
+++ Practica2.py
@@ -1,1 +1,7 @@
-·
+# Chuuya peluche
+#Ejemplo de hallar el x de una ecuación lineal, donde a y b son números ingresados por el usuario y r es el resultado de que debe dar y x el el valor que el programa debe mostrar.
+ax+b=r
+#Ejemplo de hallar el x en una ecuación cuadrática usando factorización, donde a es 1,b es 5 ,c es 6 que son números ingresados por el usuario y se debe encontrar el valor de x.
+ax^2+bx+c=0
+#Ejemplo de hallar el x en una ecuación cuadrática usando formula cuadrática donde a=2,b=3,c=-2 y debe hallar el valor de x
+x=(-b±√(b^2 - 4ac))/2a
