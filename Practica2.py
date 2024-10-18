af5ab9e40784f59f9d3309693b417dd4fe0ba0a4
Allenaig <1507828@senati.pe>
2024-10-18 04:02:28 +0000

    add:ahorasi

--- Practica2.py
+++ Practica2.py
@@ -1,5 +1,3 @@
-#chuuya peluche
-
 import math
 #Ejemplo de hallar el x de una ecuación lineal, donde a y b son números ingresados por el usuario y r es el resultado de que debe dar y x el el valor que el programa debe mostrar.
 #ax+b=r
@@ -16,6 +14,7 @@ if a != 0:
 else:
     print("a no puede ser 0")
 
+
 #Ejemplo de hallar el x en una ecuación cuadrática usando factorización, donde a es 1,b es 5 ,c es 6 que son números ingresados por el usuario y se debe encontrar el valor de x.
 #ax^2+bx+c=0
 print()
@@ -28,15 +27,21 @@ b=int(input("Ingresa un numero para b: "))
 c=int(input("Ingresa un numero para c: "))
 
 #Encontrando x
-for i in range(-abs(c), abs(c) + 1):
-    if i != 0 and c % i == 0:
-        j = c // i
-    if i + j == b:
-        x = -i
-        print("La solución es: x =",x)
-        break
+def factores(b, c):
+    for i in range(1, abs(c) + 1):
+        if c % i == 0:
+            j = c // i
+            if i + j == abs(b):
+                return -i, -j
+    return None
+
+resultado = factores(b, c)
+if resultado:
+    x1, x2 = resultado
+    print("El valor de x1=",x1 ,"y el valor de x2=",x2)
 else:
-    print("No hay resultado")
+    print("No hay solución")
+
 
 #Ejemplo de hallar el x en una ecuación cuadrática usando formula cuadrática donde a=2,b=3,c=-2 y debe hallar el valor de x
 #x=(-b±√(b^2 - 4ac))/2a
@@ -48,10 +53,15 @@ b=3
 c=-2
 print("Los valores son a=",a, "b=",b ,"y c=",c)
 
-# Encontrar X
+# Encontrando x
 discriminante = b**2 - 4*a*c
-if discriminante >= 0:
-    x = (-b + math.sqrt(discriminante)) / (2 * a)
-    print("La solución es x =",x)
+if discriminante < 0:
+    print("No hay solución.")
+elif discriminante == 0:
+    x = -b / (2*a)
+    print("La solución única es:",x)
 else:
-    print("No hay resultado")
+    x1 = (-b + math.sqrt(discriminante)) / (2*a)
+    x2 = (-b - math.sqrt(discriminante)) / (2*a)
+    print("El valor de x1=",x1, "y el valor de x2=",x2)
+
